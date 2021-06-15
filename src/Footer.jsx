768aa724361bbd9d75d185f3bab90dbc88363def
shawlycode <orlandoshawly>
2021-06-15 16:17:20 +0000

    add footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
       <div>
-      <footer style="background-color: tomato;">&copy;2021 Pizzamania All Rights Reserved</footer>
+     footer
       </div>
 
     )
